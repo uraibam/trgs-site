8a7fb0851584b591ee78f2695dee7086c99e787a
uraibam <108549524+uraibam@users.noreply.github.com>
2025-09-30 14:54:39 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-7xl px-6 py-20">
+      <p className="text-white/70">Scaffold OK. Section 1: Header will be added next.</p>
+    </main>
+  );
+}
