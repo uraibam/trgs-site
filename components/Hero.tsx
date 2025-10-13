349d330fb694c74597637db5998838d1b4c3c2aa
uraibam <108549524+uraibam@users.noreply.github.com>
2025-10-13 13:45:39 +0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
       {/* BACK STARFIELD (main) */}
       <Galaxy
         className="z-0"
-        density={1.5}
-        glowIntensity={0.48}
+        density={2.5}
+        glowIntensity={0.68}
         saturation={0.78}
         twinkleIntensity={0.3}
         starSpeed={0.5}
