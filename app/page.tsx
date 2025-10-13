10742e84d9f5ff54c4333a2dff635bbfa85eec7f
uraibam <108549524+uraibam@users.noreply.github.com>
2025-10-14 03:37:00 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import ProofSection from "../components/ProofSection";
 import Philosophy from '../components/Philosophy';
+import Journey from "../components/Journey";
 
 export default function Page() {
   return (
@@ -9,7 +10,7 @@ export default function Page() {
 
       <ProofSection />
       <Philosophy />
-      
+      <Journey />
     </main>
   );
 }
