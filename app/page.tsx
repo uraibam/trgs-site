ff654ea75a438eb948f9e87344e8aa16076cd1d4
uraibam <108549524+uraibam@users.noreply.github.com>
2025-10-14 05:26:20 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "../components/Hero";
 import ProofSection from "../components/ProofSection";
 import Philosophy from '../components/Philosophy';
 import Journey from "../components/Journey";
+import Pillars from "../components/Pillars";
 
 export default function Page() {
   return (
@@ -11,6 +12,7 @@ export default function Page() {
       <ProofSection />
       <Philosophy />
       <Journey />
+      <Pillars />
     </main>
   );
 }
